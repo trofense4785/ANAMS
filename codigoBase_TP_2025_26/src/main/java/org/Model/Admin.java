67ce4e12b05848480224e45670880570b580e99e
trofense4785 <1241095@isep.ipp.pt>
2025-11-27 11:45:25 +0000

    ATE UC7 TA FEITO

--- codigoBase_TP_2025_26/src/main/java/org/Model/Admin.java
+++ codigoBase_TP_2025_26/src/main/java/org/Model/Admin.java
@@ -3,9 +3,9 @@ package org.Model;
 public class Admin {
     private Credenciais credenciais;
 
-    public Admin(String login, String password) {
+    public Admin(String username, String password) {
         // Assume-se que o login e password são a informação necessária
-        this.credenciais = new Credenciais(login, password);
+        this.credenciais = new Credenciais(username, password);
     }
 
     // Getters and Setters
